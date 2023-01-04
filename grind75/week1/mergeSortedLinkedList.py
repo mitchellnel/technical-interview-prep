fd1f038ec7b09e9a7f5ef86f9bf415513e06c198
mitchellnel <mitchellmclin@gmail.com>
2023-01-04 20:35:39 +0800

    Actually solve Merge Sorted Linked Lists

--- grind75/week1/mergeSortedLinkedList.py
+++ grind75/week1/mergeSortedLinkedList.py
@@ -1,44 +1,39 @@
+from typing import Optional
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def printList(self):
+        current = self
+        while current is not None:
+            print(current.val)
+            current = current.next
+
+
 class Solution:
     def mergeTwoLists(
         self, list1: Optional[ListNode], list2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        head = None
-        current = None
-        while list1 != None and list2 != None:
-            print(f"list1 is {list1.val}, list2 is {list2.val}")
+        dummy = ListNode()
+        current = dummy
+        while list1 is not None and list2 is not None:
             if list1.val <= list2.val:
-                if head is None:
-                    head = list1
-                    current = list1
-                else:
-                    current.next = list1
-                    print(f"current before: {current}")
-                    current = current.next if current.next is not None else current
-                    print(f"current after: {current}")
+                current.next = list1
 
+                current = current.next
                 list1 = list1.next
             else:
-                if head is None:
-                    head = list2
-                    current = list2
-                else:
-                    current.next = list2
-                    print(f"current before: {current}")
-                    current = current.next if current.next is not None else current
-                    print(f"current after: {current}")
+                current.next = list2
 
+                current = current.next
                 list2 = list2.next
 
-        # append final parts of the list
         if list1 is not None:
             current.next = list1
         elif list2 is not None:
-            print(f"fdjs {list2}")
             current.next = list2
 
-        return head
+        return dummy.next
