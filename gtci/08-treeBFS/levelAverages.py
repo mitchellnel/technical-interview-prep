6fe0a3b4d2f978d59c069958132dc792f59f052d
mitchellnel <mitchellmclin@gmail.com>
2023-07-17 18:21:12 +0800

    Cleaner and more intuitive solution

--- gtci/08-treeBFS/levelAverages.py
+++ gtci/08-treeBFS/levelAverages.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -6,39 +9,43 @@ class TreeNode:
 
 
 def level_averages(root):
+    if root is None:
+        return []
+
     averages = []
 
-    curr_depth = 0
-
-    queue = [(root, 0)]
-    running_sum = 0
-    n_elements_in_level = 0
+    queue = deque([root])
     while len(queue) > 0:
-        curr_node, curr_node_depth = queue.pop(0)
+        level_size = len(queue)
+        level_sum = 0
 
-        # check if next node we're looking at occurs in new level
-        if curr_node_depth != curr_depth:
-            # add the average to our results list
-            averages.append(running_sum / n_elements_in_level)
+        for _ in range(level_size):
+            curr_node = queue.popleft()
 
-            # reset variables
-            running_sum = 0
-            n_elements_in_level = 0
+            # add the node's value to the running sum
+            level_sum += curr_node.val
 
-            # update current depth
-            curr_depth = curr_node_depth
+            # add the node's children to the queue
+            if curr_node.left is not None:
+                queue.append(curr_node.left)
+            if curr_node.right is not None:
+                queue.append(curr_node.right)
 
-        # append children to end of queue
-        if curr_node.left is not None:
-            queue.append((curr_node.left, curr_depth + 1))
-        if curr_node.right is not None:
-            queue.append((curr_node.right, curr_depth + 1))
-
-        # add to the running sum
-        running_sum += curr_node.val
-        n_elements_in_level += 1
-
-    # add the average for the last level
-    averages.append(running_sum / n_elements_in_level)
+        # append the current level's average to our averages array
+        averages.append(level_sum / level_size)
 
     return averages
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.left.right = TreeNode(2)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Level averages are: " + str(level_averages(root)))
+
+
+main()
