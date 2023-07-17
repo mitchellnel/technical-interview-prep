5174aa5bac0a28fcda748d0c541935e85ebf2a75
mitchellnel <mitchellmclin@gmail.com>
2023-07-17 19:05:48 +0800

    Use `deque` and add main routines for testing

--- gtci/08-treeBFS/minimumDepth.py
+++ gtci/08-treeBFS/minimumDepth.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -6,10 +9,13 @@ class TreeNode:
 
 
 def minimum_depth(root):
-    queue = [(root, 0)]
+    if root is None:
+        return 0
+
+    queue = deque([(root, 1)])
 
     while len(queue) > 0:
-        curr_node, curr_node_depth = queue.pop(0)
+        curr_node, curr_node_depth = queue.popleft()
 
         # when we run into our first leaf node, its depth is the min depth
         if curr_node.left is None and curr_node.right is None:
@@ -20,3 +26,20 @@ def minimum_depth(root):
             queue.append((curr_node.left, curr_node_depth + 1))
         if curr_node.right is not None:
             queue.append((curr_node.right, curr_node_depth + 1))
+
+    return -1
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Tree Minimum Depth: " + str(minimum_depth(root)))
+    root.left.left = TreeNode(9)
+    root.right.left.left = TreeNode(11)
+    print("Tree Minimum Depth: " + str(minimum_depth(root)))
+
+
+main()
--- gtci/08-treeBFS/reverseLevelOrderBinaryTreeTraversal.py
+++ gtci/08-treeBFS/reverseLevelOrderBinaryTreeTraversal.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class TreeNode:
     def __init__(self, val):
         self.val = val
@@ -10,9 +13,9 @@ def reverse_level_order_traversal(root):
 
     curr_depth = -1
 
-    queue = [(root, 0)]
+    queue = deque([(root, 0)])
     while len(queue) > 0:
-        curr_node, curr_node_depth = queue.pop(0)
+        curr_node, curr_node_depth = queue.popleft()
 
         # check if next node we're looking at occurs in new level
         if curr_node_depth != curr_depth:
@@ -32,3 +35,16 @@ def reverse_level_order_traversal(root):
         traversal[0].append(curr_node.val)
 
     return traversal
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    print("Reverse level order traversal: " + str(reverse_level_order_traversal(root)))
+
+
+main()
